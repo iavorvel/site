93b3dff248580653133af4f2b31e0ae30c3fe2e0
Iavor Veltchev <175457104+iavorvel@users.noreply.github.com>
2025-03-31 11:12:53 -0400

    MyLD2410 V1.2.2

--- MyLD2410/search/all_0.js
+++ MyLD2410/search/all_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['autostatus_0',['AutoStatus',['../MyLD2410_8h.html#a75298cc21a86826776ac6cf137ffb791',1,'MyLD2410.h']]],
-  ['autothresholds_1',['autoThresholds',['../classMyLD2410.html#a5a1bc0cde666af70f1e311cd2dc204ff',1,'MyLD2410']]]
+  ['autothresholds_1',['autoThresholds',['../classMyLD2410.html#a3bca7f40573abaefe6c658b120c72d71',1,'MyLD2410']]]
 ];
--- MyLD2410/search/functions_0.js
+++ MyLD2410/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['autothresholds_0',['autoThresholds',['../classMyLD2410.html#a5a1bc0cde666af70f1e311cd2dc204ff',1,'MyLD2410']]]
+  ['autothresholds_0',['autoThresholds',['../classMyLD2410.html#a3bca7f40573abaefe6c658b120c72d71',1,'MyLD2410']]]
 ];
